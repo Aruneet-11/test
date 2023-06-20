cd01b7c7dd3b6438709b6613d3a4e736279cece6
Aruneet <87275550+Aruneet-11@users.noreply.github.com>
2023-06-20 11:14:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+document.getElementById("myprojects").onclick=function()
+{
+    location.href="projects.html";
+}
+document.getElementById("myproject-1").onclick=function()
+{
+    location.href="https://aruneet-11.github.io/Foodorder/";
+}
+document.getElementById("myproject-2").onclick=function()
+{
+    location.href="https://aruneet-11.github.io/drumKit/";
+}
